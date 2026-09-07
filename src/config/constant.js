94fec782d4d843a6189f63c0a528d5586474fd02
leoTechHub <chinwekeleuchenna@gmail.com>
2026-09-07 06:33:06 +0100

    jhfjd

---
+++ src/config/constant.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const envConfig = {
+    DB_HOST: process.env.DB_HOST,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_NAME: process.env.DB_NAME,
+    DB_PORT: process.env.DB_PORT,
+}
+
+export default envConfig;
