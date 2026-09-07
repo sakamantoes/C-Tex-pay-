04475dc6443ee2e118b6d4e502338ee30b704cf2
leoTechHub <chinwekeleuchenna@gmail.com>
2026-09-07 09:41:20 +0100

    jjdk

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  }),
+);
 
 app.get("/", (req, res) => {
   res.json({
-    message: "API is running",
+    message: "C_TEX_PAY API is running",
   });
 });
 
